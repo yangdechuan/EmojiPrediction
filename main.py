e50d9589661a965c9bd07a5c2d5e8208a13372f2
captain <yangdechuan@hotmail.com>
2018-07-30 22:17:32 +0800

    add logging

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import os
+import logging
+
 from src.train import svm
+
+def config_log():
+    """Config logging."""
+    s_handler= logging.StreamHandler()
+    s_handler.setLevel(logging.INFO)
+    info_handler = logging.FileHandler(filename=os.path.join("output", "log", "info.log"), mode="w", encoding="utf-8")
+    info_handler.setLevel(level=logging.INFO)
+    err_handler = logging.FileHandler(filename=os.path.join("output", "log", "error.log"), mode="w", encoding="utf-8")
+    err_handler.setLevel(logging.ERROR)
+
+    logging.basicConfig(level=logging.INFO,
+                        datefmt="%H:%M:%M",
+                        format="{asctime} [{levelname}]>> {message}",
+                        style="{",
+                        handlers=[s_handler, info_handler, err_handler])
 
 
 def main():
+    config_log()
     svm()
 
 
--- src/svm.py
+++ src/svm.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -17,23 +18,31 @@ class SVMModel(object):
         self.model = None
 
     def load_data(self):
+        logging.info("Loading data...")
         self.x_train, self.y_train = load_json_data(os.path.join("data", "us_train.json"))
         self.x_test, self.y_test = load_json_data(os.path.join("data", "us_test.json"))
         self.x_valid, self.y_valid = load_json_data(os.path.join("data", "us_valid.json"))
+        logging.info("train data size: {}".format(len(self.x_train)))
+        logging.info("test data size: {}".format(len(self.x_test)))
+        logging.info("Load data success!")
 
         # Use TF-IDF features.
-        tfidf = TfidfVectorizer(ngram_range=(1,2))
+        logging.info("Building sentences tfidf features...")
+        tfidf = TfidfVectorizer(ngram_range=(1, 1))
         self.x_train = tfidf.fit_transform(self.x_train)
         self.x_test = tfidf.transform(self.x_test)
         self.x_valid = tfidf.transform(self.x_valid)
+        logging.info("Build features success!")
 
     def build_model(self):
         self.model = LinearSVC()
 
     def train_model(self):
+        logging.info("Training model...")
         self.model.fit(self.x_train, self.y_train)
 
     def test_model(self):
+        logging.info("Testing model:")
         y_pred = self.model.predict(self.x_test)
         f1_score = metrics.f1_score(self.y_test, y_pred, average="macro")
         accuracy = metrics.accuracy_score(self.y_test, y_pred)
